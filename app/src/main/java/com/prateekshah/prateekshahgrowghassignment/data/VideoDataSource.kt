7e69dbed61b14a3153ccb7b3680e9d79413153ee
Prateek Shah <shahprateek02@gmail.com>
2023-07-31 11:28:58 +0530

    Added Some Components to the Feeds Fragment and Used to ViewPager2 for Video Fragment

--- app/src/main/java/com/prateekshah/prateekshahgrowghassignment/data/VideoDataSource.kt
+++ app/src/main/java/com/prateekshah/prateekshahgrowghassignment/data/VideoDataSource.kt
@@ -3,11 +3,11 @@ package com.prateekshah.prateekshahgrowghassignment.data
 class VideoDataSource {
     fun loadVideoIds(): List<VideoModel> {
         return listOf(
-            VideoModel("https://vod.api.video/vod/vi32GHjHdRADU7s7vctDkU79/mp4/source.mp4", "Title1", "Desc of Video1"),
-            VideoModel("https://vod.api.video/vod/vi5URIIWoql0wrEWcyU4W80/mp4/source.mp4", "Title2", "Desc of Video2"),
-            VideoModel("https://vod.api.video/vod/vim8Z5QJqqQ9I03bjKVRFtQ/mp4/source.mp4", "Title3", "Desc of Video3"),
-            VideoModel("https://vod.api.video/vod/vi43E1dP5betkQdbnYiwQRCT/mp4/source.mp4", "Title4", "Desc of Video4"),
-            VideoModel("https://vod.api.video/vod/vi7fFopGLNjHmfufDjdVYQyh/mp4/source.mp4", "Title5", "Desc of Video5"),
+            VideoModel("https://vod.api.video/vod/vi5Ip5AUurYonZrmy1DEpyk/mp4/source.mp4", "Title1", "Desc of Video1"),
+            VideoModel("https://vod.api.video/vod/vi7QYzMKMGB02UjdLnN2QN9Z/mp4/source.mp4", "Title2", "Desc of Video2"),
+            VideoModel("https://vod.api.video/vod/vi2QqeQn62NcKviLaEdBNxmN/mp4/source.mp4", "Title3", "Desc of Video3"),
+            VideoModel("https://vod.api.video/vod/vi1ECSpzymGEx3m5MJ5wpY1U/mp4/source.mp4", "Title4", "Desc of Video4"),
+            VideoModel("https://vod.api.video/vod/vi7Fyzci0D1NygNYqVR4VxvH/mp4/source.mp4", "Title5", "Desc of Video5"),
         )
     }
 }
